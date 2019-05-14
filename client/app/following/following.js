a096df3f24f8b2c0a84e85ef820316a110dcd22c
JirapasChuatrakul <alfa3225@hotmail.co.th>
2019-05-14 23:35:24 +0700

    Add Follow/UnFollow Function to angular client

--- client/app/following/following.js
+++ client/app/following/following.js
@@ -7,11 +7,25 @@ angular.module('followingList', ['ngRoute'])
       var self = this;
 
       const requestOptions = {
-          headers: { 'X-session': $rootScope.x_session }
+          headers: { 'X-session': $rootScope.x_session, "Content-Type": "application/json;charset=utf-8"}
       };
 
       $http.get('http://localhost:8080/twitterapi/following/', requestOptions).then(function (response) {
         self.followings = response.data;
       });
+	  self.sendFollow = function sendFollow(followingname){
+	  	  const data = "followingname=" + encodeURIComponent(followingname);
+		  $http.post('http://localhost:8080/twitterapi/following/', data, requestOptions).then(function (response){
+		  console.log(response);
+		  });
+	  }
+	  self.sendUnFollow = function sendUnFollow(followingname){
+	  	  $http.defaults.headers.delete = { 'X-session': $rootScope.x_session , "Content-Type": "application/json;charset=utf-8"};
+		  const data = "followingname=" + encodeURIComponent(followingname);
+		  console.log(data);
+		  $http.delete('http://localhost:8080/twitterapi/following/?' + data).then(function (response){
+		  console.log(response);
+		  });
+	  }
     }]
 });
